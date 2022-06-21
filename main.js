c9c682b00f9af8da8e32bd977ccd0d7e93b50f25
Angela Vegega <angela.vegega@gmail.com>
2022-06-21 22:59:54 +0900

    jquery finished

---
+++ main.js
@@ -0,0 +1,34 @@
+const { response } = require("express")
+
+$(document).ready(function () {
+  $('#title').autocomplete({
+    source: async function(req, res) {
+      let data = await fetch(`http://localhost:8000/search?query=${req.term}`)
+        .then(results => results.json())
+        .then(results => results.map(result => {
+          return {
+            label: result.title,
+            value: result.title,
+            id: result._id
+          }
+        }))
+      response(data)
+    },
+    minLength: 2,
+    select: function(event, ui) {
+      console.log(ui.item.id)
+      fetch(`http://localhost:8000/get/${ui.item.id}`)
+        .then(result => result.json())
+        .then(result => {
+          $('#cast').empty()
+          result.cast.forEach(cast => 
+          {
+            $(cast).append(`<li>${cast}</li>`)
+          })
+          $('img').attr('src',result.poster)
+        })
+    }
+})
+}
+
+)
